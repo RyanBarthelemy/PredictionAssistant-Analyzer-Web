95dda36a5476684ce87e3deda4024c1bc4a3a31c
axlor <axlor852@gmail.com>
2020-06-10 15:15:17 -0500

    Accidentally made navigation a component rather than a directory to hold navigation components, removed navigation component files so it is just a directory now.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,11 @@ import { MarketsAvailableComponent } from './pages/markets-available/markets-ava
 import { MarketDataComponent } from './pages/market-data/market-data.component';
 import { ContractHistoryComponent } from './pages/contract-history/contract-history.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavigationComponent } from './navigation/navigation.component';
 import { HeaderComponent } from './navigation/header/header.component';
 import { NavtabsComponent } from './navigation/navtabs/navtabs.component';
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 import {AppRoutingModule} from './app-routing.module';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
@@ -22,7 +22,6 @@ import {AppRoutingModule} from './app-routing.module';
     MarketsAvailableComponent,
     MarketDataComponent,
     ContractHistoryComponent,
-    NavigationComponent,
     HeaderComponent,
     NavtabsComponent,
     SidenavListComponent
@@ -30,7 +29,8 @@ import {AppRoutingModule} from './app-routing.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
