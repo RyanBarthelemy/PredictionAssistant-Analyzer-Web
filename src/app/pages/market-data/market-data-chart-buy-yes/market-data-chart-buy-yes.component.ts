9fccf3d95798c4b2fa6d5d9ccb42725f268d2ca6
axlor <axlor852@gmail.com>
2020-06-22 15:34:20 -0500

    Changed x-axis data point to show how old the data point is versus the most recent rather than an unreadable timestamp.

--- src/app/pages/market-data/market-data-chart-buy-yes/market-data-chart-buy-yes.component.ts
+++ src/app/pages/market-data/market-data-chart-buy-yes/market-data-chart-buy-yes.component.ts
@@ -47,19 +47,35 @@ export class MarketDataChartBuyYesComponent implements OnInit {
   }
 
   private buildDataSet() {
-    const lineChartData: ChartDataSets[] = [];
     const labels = [];
 
     const priceArray = [];
     const dataSetLabel = this.marketHistory[0].contracts[0].name;
     this.lineChartLabels = [];
+    const latestSnapshotTime = new Date(this.marketHistory[0].timeStamp.toString().replace('T', ' ')).getTime();
+
     // tslint:disable-next-line:prefer-for-of
     for (let i = 0; i < this.marketHistory.length; i++) {
       priceArray.push(this.marketHistory[i].contracts[0].bestBuyYesCost);
-      labels.push(this.marketHistory[i].timeStamp);
+      const timestamp: Date = new Date(this.marketHistory[i].timeStamp.toString().replace('T', ' '));
+      const timeDifferenceMinutes = (latestSnapshotTime - timestamp.getTime()) / (1000 * 60);
+
+      // okay so converting UTC Date given by PredictIt to the local user's time zone that also correctly accounts for DST is unnecessarily
+      //  complicated. I also think it is easier to use the chart if data points are show how old the data is rather than a timestamp
+      // the following converts shows how old the data point is in minutes if less than an hour, or hours old otherwise.
+      if (timeDifferenceMinutes < 60){
+        labels.push(timeDifferenceMinutes.toFixed(0).toString() + ' mins ago');
+      }
+      else{
+        const timeDifferenceHours = (latestSnapshotTime - timestamp.getTime()) / (1000 * 60 * 60);
+        labels.push(timeDifferenceHours.toFixed(1).toString() + ' hours ago');
+      }
     }
+
+    // we have to reverse these so the chart reads from left(oldest data) to right(newest data).
     this.lineChartLabels = labels.reverse();
     this.lineChartData[0].data = priceArray.reverse();
+
     this.lineChartData[0].label = dataSetLabel;
 
   }
