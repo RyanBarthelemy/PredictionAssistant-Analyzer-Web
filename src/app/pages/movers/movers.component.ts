2eb8c5cabdbb0b44992876a473aae416723a76fe
axlor <axlor852@gmail.com>
2020-07-03 15:16:37 -0500

    Tried to clean up some old and unnecessary logs and stuff.

--- src/app/pages/movers/movers.component.ts
+++ src/app/pages/movers/movers.component.ts
@@ -30,36 +30,31 @@ export class MoversComponent implements OnInit {
     this.timeframe = this.DEFAULT_TIMEFRAME;
     of(this.timeframe).subscribe(
       timeframe => this.buildPage(),
-      error => console.log(error)
+      error => this.buildError(error)
     );
   }
 
   private buildError(error) {
-    console.log('error: ' + error);
+    console.log('error: ' + error); // setup elements needed to build an error page in html
+    // this should basically never happen, only way it could happen is if call to backend API times out for w/e reason.
   }
 
   private getEarlierSnapId(snapshotMinis: SnapshotMini[]) {
-    const timeFrameMilli = this.timeframe * 60 * 1000;
-    // console.log('timeFrameMilli: ' + timeFrameMilli);
+    const timeFrameMilli = this.timeframe * 60 * 1000; // convert minutes timeframe to usable milliseconds
     let timeDiff = 999999999999;
     let indexToUse = -1;
-    // console.log(snapshotMinis[0].timestampDisplay);
+
     // tslint:disable-next-line:prefer-for-of
     for (let i = 0; i < snapshotMinis.length; i++) {
       const timeDiffCurrent = Math.abs(this.currentSnapshot.timestamp - (new Date(snapshotMinis[i].timestampDisplay).getTime()));
-      // console.log(i + ': timeDiffCurrent: ' + timeDiffCurrent);
-      // console.log(Math.abs(timeDiffCurrent - timeFrameMilli));
-      // console.log('TimeDifference: ' + Math.abs(timeDiffCurrent - timeFrameMilli));
       if (Math.abs(timeDiffCurrent - timeFrameMilli) < timeDiff) {
         timeDiff = Math.abs(timeDiffCurrent - timeFrameMilli);
         indexToUse = i;
-        // console.log('Updated indexToUse to = ' + indexToUse);
       }
     }
     if (indexToUse === -1) {
       return null;
     }
-    // console.log('earlier snapshot hashId: ' + snapshotMinis[indexToUse].hashID);
     return snapshotMinis[indexToUse].hashID;
   }
 
@@ -71,28 +66,23 @@ export class MoversComponent implements OnInit {
       // the market may not be contained in the earlier snapshot, ie the market may be new or something, so we need to check.
       const indexOfMarketInEarlierSnapshot = this.getMarketIndexById(this.earlierSnapshot, this.currentSnapshot.markets[i].id);
       if (indexOfMarketInEarlierSnapshot === null) {
-        // console.log('indexOfMarketInEarlierSnapshot is NULL');
         continue;
       }
       const marketNow: Market = this.currentSnapshot.markets[i];
       const marketPrev: Market = this.earlierSnapshot.markets[indexOfMarketInEarlierSnapshot];
 
-      // console.log(marketNow.timeStamp + ' --- ' + marketPrev.timeStamp);
-      // console.log('MarketID: ' + marketPrev.id + ' -- NumContracts: ' + marketNow.contracts.length);
       // tslint:disable-next-line:prefer-for-of
       for (let j = 0; j < marketNow.contracts.length; j++) {
         const indexOfContractInEarlierMarket = this.getContractIndexById(marketPrev, marketNow.contracts[j].id);
+
         if (indexOfContractInEarlierMarket === null) {
-          console.log('indexOfContractInEarlierMarket is NULL');
           continue;
         }
         const contractNow: Contract = marketNow.contracts[j];
         const contractPrev: Contract = marketPrev.contracts[indexOfContractInEarlierMarket];
-
         const priceDifference = contractNow.bestBuyYesCost - contractPrev.bestBuyYesCost;
-        // console.log(priceDifference);
+
         if (Math.abs(priceDifference) > 0) {
-          // console.log(priceDifference.toFixed(2) + ' -- ' + contractNow.name + ' -- ' + marketNow.name);
           const mover: Mover = {
             marketName: marketNow.name,
             contractName: contractNow.name,
@@ -101,6 +91,7 @@ export class MoversComponent implements OnInit {
             movement: Math.round((priceDifference * 100)),
             timeframe: this.timeframe
           };
+
           if (mover.marketName === mover.contractName){
             mover.contractName = 'Yes/No';
           }
@@ -122,51 +113,36 @@ export class MoversComponent implements OnInit {
   private getMarketIndexById(earlierSnapshot: Snapshot, id: number) {
     // tslint:disable-next-line:prefer-for-of
     for (let i = 0; i < earlierSnapshot.markets.length; i++) {
-      if (earlierSnapshot.markets[i].id === id) {
-        return i;
-      }
+      if (earlierSnapshot.markets[i].id === id) { return i; }
     }
     return null;
   }
 
   private getContractIndexById(marketPrev: Market, id: number) {
     for (let i = 0; i < marketPrev.contracts.length; i++) {
-      if (marketPrev.contracts[i].id === id) {
-        return i;
-      }
+      if (marketPrev.contracts[i].id === id) { return i; }
     }
     return null;
   }
 
   public timeframeButtonClicked() {
-    console.log('button click test -- ' + this.timeframeInputField);
-    if (isNaN(this.timeframeInputField)){
-      console.log(this.timeframeInputField + ' is not a number...');
-      return;
+    if (!isNaN(this.timeframeInputField)){
+      this.timeframe = this.timeframeInputField;
+      console.log('this.timeframe = ' + this.timeframe);
+      this.buildPage();
     }
-    this.timeframe = this.timeframeInputField;
-    console.log('this.timeframe = ' + this.timeframe);
-    this.buildPage();
   }
 
   private buildPage() {
-    console.log('buildPage called');
     this.dataService.getAllSnapshots().subscribe(
       (snapshotMinis: SnapshotMini[]) => {
-        // console.log(snapshotMinis.length);
         this.dataService.getSnapshot(snapshotMinis[0].hashID).subscribe(
           (snapshotCurrent: Snapshot) => {
             this.currentSnapshot = snapshotCurrent;
-            // console.log('current snapshot timestamp: ' + new Date(snapshotCurrent.timestamp));
             const earlierSnapshotHashId = this.getEarlierSnapId(snapshotMinis);
-            // console.log('Earlier Snapshot HashID: ' + earlierSnapshotHashId);
-            // console.log('Current Snapshot HashID: ' + this.currentSnapshot.hashId);
             this.dataService.getSnapshot(earlierSnapshotHashId).subscribe(
               (snapshotEarlier: Snapshot) => {
                 this.earlierSnapshot = snapshotEarlier;
-                // console.log('Earlier Timestamp: ' + this.earlierSnapshot.timestamp);
-                // console.log('Most Recent Timestamp:' + this.currentSnapshot.timestamp);
-                // console.log('Time Difference: ' + (this.currentSnapshot.timestamp - this.earlierSnapshot.timestamp));
                 this.calculateMovers();
               },
               error => this.buildError(error)
